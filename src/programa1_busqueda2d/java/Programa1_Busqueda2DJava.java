8ae0ffabecc5559c394dcf6a4766537596d5fb7a
yuliana-rodriguezm <ye.rodriguezm@uea.edu.ec>
2025-09-01 18:41:52 -0500

    Update Programa1_Busqueda2DJava.java
    Corrección

--- src/programa1_busqueda2d/java/Programa1_Busqueda2DJava.java
+++ src/programa1_busqueda2d/java/Programa1_Busqueda2DJava.java
@@ -1,36 +1,37 @@
 package programa1_busqueda2d.java;
 public class Programa1_Busqueda2DJava {
     public static void main(String[] args) {
-        int[][] matriz = {
+int[][] matriz = {
                 {5, 8, 3},
-                {2, 9, 4},
-                {7, 1, 6}
+                {2, 1, 4},
+                {7, 9, 6}
         };
+        //Valor que vamos a buscar
+        int valorBuscado = 6;
 
-        int valorBuscado = 9;
-        int[] posicion = buscarValor(matriz, valorBuscado);
+        //Rastrear la posición
+        int filaEncontrada = -1;
+        int columnaEncontrada = -1;
 
-        if (posicion[0] != -1) {
-            System.out.println("El valor " + valorBuscado +
-                    " se encontro en la posicion [fila: " + posicion[0] +
-                    ", columna: " + posicion[1] + "]");
-        } else {
-            System.out.println("El valor " + valorBuscado + " no se encontró en la matriz.");
-        }
-    }
-    
-    public static int[] buscarValor(int[][] matriz, int valor) {
-        for (int fila = 0; fila < matriz.length; fila++) {
-            for (int col = 0; col < matriz[fila].length; col++) {
-                if (matriz[fila][col] == valor) {
-                    return new int[]{fila, col};
+        outer: //romper el ciclo externo
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == valorBuscado) {
+                    filaEncontrada = i;
+                    columnaEncontrada = j;
+                    break outer; //detiene la búsqueda
                 }
             }
         }
-        return new int[]{-1, -1};
+
+       // Muestra los resultados
+        if (filaEncontrada != -1) {
+            System.out.println("El valor " + valorBuscado +
+                    " se encontró en la posición [fila: " +
+                    filaEncontrada + ", columna: " + columnaEncontrada + "]");
+        } else {
+            System.out.println("El valor " + valorBuscado +
+                    " no se encontró en la matriz.");
+        }
     }
 }
-
-   
-    
-
