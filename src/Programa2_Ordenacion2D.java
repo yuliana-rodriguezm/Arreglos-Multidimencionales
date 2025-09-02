30062f94602fd727b0c95c2ad8024988f300e787
yuliana-rodriguezm <ye.rodriguezm@uea.edu.ec>
2025-09-01 19:03:38 -0500

    Update Programa2_Ordenacion2D.java
    corrección

--- src/Programa2_Ordenacion2D.java
+++ src/Programa2_Ordenacion2D.java
@@ -2,38 +2,27 @@ import java.util.Arrays;
 
 public class Programa2_Ordenacion2D {
     public static void main(String[] args) {
-        int[][] matriz = {
-                {9, 4, 7},
-                {3, 8, 1},
-                {6, 2, 5}
-        };
 
-        int filaAOrdenar = 1;
+        // Declarar matriz 3x3
+        int[][] matriz = {
+            {4, 7, 2},
+            {9, 3, 6},
+            {8, 1, 5}
+        };
 
         System.out.println("Matriz original:");
         mostrarMatriz(matriz);
-
-        ordenarFila(matriz, filaAOrdenar);
+        // ordenar
+        int filaAOrdenar = 1;
+        Arrays.sort(matriz[filaAOrdenar]);
 
         System.out.println("\nMatriz con la fila " + filaAOrdenar + " ordenada:");
         mostrarMatriz(matriz);
     }
-
-    public static void ordenarFila(int[][] matriz, int fila) {
-        if (fila >= 0 && fila < matriz.length) {
-            Arrays.sort(matriz[fila]); 
-        } else {
-            System.out.println("Fila no válida.");
-        }
-    }
-
+    
     public static void mostrarMatriz(int[][] matriz) {
         for (int[] fila : matriz) {
-            for (int valor : fila) {
-                System.out.print(valor + " ");
-            }
-            System.out.println();
+            System.out.println(Arrays.toString(fila));
         }
     }
 }
-
